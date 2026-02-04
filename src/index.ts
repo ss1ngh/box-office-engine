a7c7632577d2a5554522ccdb7f57b1cce61f9752
ss1ngh <singhshreyansh2354@gmail.com>
2026-02-04 22:31:50 +0530

    refactor code

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
         method : req.method
     });
 
-    const statusCode = err.statusCode ||err.status || 500; 
+    const statusCode = err.statusCode || err.status || 500; 
     const message = err.message || "Internal Server Error";
 
     return res.status(statusCode).json({
--- src/routes/v1/movie.routes.ts
+++ src/routes/v1/movie.routes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { MovieController } from '../../controllers';
+import {validateBody } from '../../middlewares/validate';
+import { createMovieSchema } from '../../schema/movie.schema';
 const router = express.Router();
 
-router.post('/', MovieController.addMovie);
+router.post('/', validateBody(createMovieSchema),MovieController.addMovie);
 router.delete('/', MovieController.deleteMovie);
 router.get('/:id', MovieController.getMovie);
 router.get('/', MovieController.getAllMovies);
