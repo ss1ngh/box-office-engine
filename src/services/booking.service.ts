4b46882f00bb5f6922822d61a44e6985905dd0b2
ss1ngh <singhshreyansh2354@gmail.com>
2026-02-07 15:25:29 +0530

    implement secure booking creation login

---
+++ src/services/booking.service.ts
@@ -0,0 +1,64 @@
+import {prisma, Logger} from '../config';
+import { Decimal } from '@prisma/client/runtime/library';
+
+export const createBooking = async(
+    userId : number,
+    showtimeId : number,
+    seatIds : number[],
+) => {
+    Logger.info(`BookingService : createBooking : User ${userId} booking seats [${seatIds.join(', ')}] for showtime ${showtimeId}`);
+
+    return await prisma.$transaction(async (tx) => {
+
+        const showtime = await tx.showtime.findUnique({
+            where  : {showtimeId}
+        });
+
+        if(!showtime) {
+            Logger.error(`BookingService : createBooking : Showtime not found: ${showtimeId}`);
+
+            throw Object.assign(new Error('Showtime not found'), { statusCode: 404 });
+        }
+
+
+        const existingTickets = await tx.ticket.findMany({
+            where : {
+                showtimeId,
+                seatId : {in : seatIds}
+            }
+        });
+
+        if(existingTickets.length > 0) {
+            const bookedSeats = existingTickets.map(t => t.seatId);
+            Logger.warn(`BookingService : createBooking : Seats already booked: [${bookedSeats.join(', ')}]`);
+            throw Object.assign( new Error('One or more seats are already booked'), {statusCode : 409});
+        }
+
+        //calculate total
+        const total = new Decimal(showtime.price.toString()).mul(seatIds.length);
+        Logger.info(`BookingService : createBooking : Total calculated: ${total}`);
+
+        //create booking
+        const booking = await tx.booking.create({
+            data : {
+                userId,
+                total,
+                status : "PENDING",
+
+                tickets : {
+                    create : seatIds.map(seatId => ({
+                        showtimeId,
+                        seatId
+                    }))
+                }
+            },
+            include : {
+                tickets : {
+                    include : {seat : true, showtime: { include : {movie : true} } }
+                }
+            }
+        });
+        Logger.info(`BookingService : createBooking : Booking created with ID: ${booking.bookingId}`);
+        return booking;
+    });
+}
