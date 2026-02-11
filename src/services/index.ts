8ba3b5f8dea58ef883b1401c2acc835183e510e2
ss1ngh <singhshreyansh2354@gmail.com>
2026-02-11 19:48:54 +0530

    add screen service export to services index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,4 @@
 export * as MovieService from './movies.service';
 export * as AuthService from './auth.service';
 export * as TheaterService from './theater.service';
+export * as ScreenService from './screen.service';
