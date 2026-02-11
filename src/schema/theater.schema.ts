ed522c2f7fb6e0c0c7014091772f28a93b6e7109
ss1ngh <singhshreyansh2354@gmail.com>
2026-02-11 23:28:48 +0530

    feat(theater) :zod schema

--- src/schema/theater.schema.ts
+++ src/schema/theater.schema.ts
@@ -1,4 +1,4 @@
-import {z} from 'zod';
+import { z } from 'zod';
 
 export const createTheaterSchema = z.object({
     name: z.string().min(1, 'Theater name is required'),
@@ -6,10 +6,9 @@ export const createTheaterSchema = z.object({
 });
 
 export const updateTheaterSchema = z.object({
-    name : z.string().min(1).optional(),
-    location : z.string().min(1).optional(),
+    name: z.string().min(1).optional(),
+    location: z.string().min(1).optional(),
 });
-
 
 export type CreateTheaterInput = z.infer<typeof createTheaterSchema>;
 export type UpdateTheaterInput = z.infer<typeof updateTheaterSchema>;
